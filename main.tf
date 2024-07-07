7bc2243e6fb9573b980a54aac0c82995be38831e
tr23malyarchuk <malyarchuk.bogdan@lll.kpi.ua>
2024-07-07 05:43:44 +0300

    commit #38: test commit

--- main.tf
+++ main.tf
@@ -107,13 +107,12 @@ resource "github_repository_file" "codeowners" {
 * @softservedata
 EOF
   commit_message = "Add CODEOWNERS file"
-  branch         = "main"
 }
 
 variable "deploy_key_public_key" {
   description = "Deploy Key Public Key"
   type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCw2CwBPsGNuW4nsrm/9Ze1vkxY7L+jdfHnCykltAnBNy09yYSX9L0+DjYWmxwcm5Dgu8q8gMHC05SUYhvCKCMMjai34Wrd7wtRAamXnZ0cXQFArVuQe9bEZcH/dukEm2cwHxb7kcf2x7JE3hGg/wwOtcuSMhHtmWq5p8xHXrsUUfJ20UFW+f7OeCWSbBeRFfUR/QHkVk9TSbrUX+4N0sWAhatQRNDQ4HoAfkC6Zip9Dmmm35sR7CWaz3oqIWrOxVCNszP1SxViRfs4CyFCMzZuDcafWPLm5KiJ4L26ClqCNIoociKUMZXwgUCoQiscLUe25gZWTu2kbTJ1BKC9Y82nVtDqpB0E6VdrbkCEIbP4c/uiFLYDDrqlrk3+bdAoMUy9Ph4oqUYELc0E69jpWRFOxhwkKFknQ3X7i2qnSBQV0GTsgRIAD0hQ4ESgHJoTCrrWfsGJLJGAdpUgQrnCHz66JS3H7ewoDEqBFyCqb/Bzy8tdxwv91MCjRb8a41ZVi9/ONUP1XE2Xje0a+2Mrfi4tLPTPV3xzQbl9GijaH+JBMZ2iLpeCHRYyPi1fvEOQDcDebr+P3nBLYKNraawxwMgmKDvbYR2cCq89j6HXNJ8u9jnXfUJcvbISNPfI21GX8vjW+Sb9ye2n0mMUTJgcICqE2B2+gMjm58Yk4f0J7TD4pQ== malyarchuk.bogdan@lll.kpi.ua"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCliadMujs5abAHxToy/iE6NihG8dZTVXw8xRsIs0+HEP6DvqhVaMjmzNft3M4oo6Cb3EULt5ZYKSWA/oRamPt7qrCp8sRi2s9azhNAwkVuyAKzR8mq2iacK1rsqQeYgmxlpj6iHB6X3yDRG4vm+KLbFiv8Vpbv7QsbVMwW7Q0kHVEI5E7xCLxXwPBicNCH8mwpoR82BNz3+e814vIRMBH3te28iHDYLkmB2P/VVB1AF0zwXt4i6FqlLEY16x7iX5qkGZt54C2hL5Mff0EDW3J37OMsY9amcM7gGFp2tS+EX3Mp7Qqr+m04DUKKdfUXnNbTEs2ns26E8GnkXZ/86o5yKkj3nfd+WzcFvau/wdpv7Hw0RhkwwcQbUA03QBpEj3obO57tBsR/ZLBo+YkJ2A+l3WOY0nGCJ7zSfmwEPA0px5gQNdOCrSG57hbxCEDbwsmfpfpePw0UfVEZSCNkSVgvSK+TxZFokNmdHU27l8guPfKyS13rtILIYkjibOQXASDCfJryQQnw4xrwaWIiNqQJzTQLki1TlouVOWX6MY/FrNMPKJwwoFSqf/Ck4yA3l9RAu/NmKhTaIAcqc0qy/QA4xYU5qGffw5Pk3+OXioVJC7/2gFL7/vAuZMPJVOF2omzoR4mr8OOuQkZ2Xpcze1u7hPN7qe1RvqfsUOLanxRNBw== malyarchuk.bogdan@lll.kpi.ua"
 }
 
 variable "discord_team_id" {
